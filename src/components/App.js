c19ac97baa3d8936c7317a005a35462f9ea60aad
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-20 22:18:45 -0400

    finally finished it

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [dogs, setDogs] = useState([])
+  const [dogClicked, setDogClicked] = useState({})
+  const [goodDogFilter, setGoodDogFilter] = useState(false)
+  const dogsURL = "http://localhost:3001/pups"
+
+  useEffect(() => {
+    fetch(dogsURL)
+      .then(resp => resp.json())
+      .then(data => setDogs(data))
+  }, [])
+
+  const handleDogClick = (id) => {
+    setDogClicked(dogs.find(dog => id === dog.id))
+  }
+
+  const handleUpdateIsGood = () => {
+    const configObj = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        ...dogClicked,
+        isGoodDog: !dogClicked.isGoodDog
+      })
+    }
+
+    fetch(`${dogsURL}/${dogClicked.id}`, configObj)
+      .then(resp => resp.json())
+      .then(data => {
+        const updatedDogs = dogs.map(dog => {
+          if (dog.id === dogClicked.id) {return data}
+          else {return dog}
+        })
+
+        setDogs(updatedDogs)
+        setDogClicked(data)
+      })
+  }
+
+  const handleUpdateGoodDogFilter = () => {
+    setGoodDogFilter((goodDogFilter) => !goodDogFilter)
+  }
+
+  const dogBarItems = dogs.map(dog => {
+    if (goodDogFilter === false || dog.isGoodDog === true) {
+      return <span key={dog.id} onClick={() => handleDogClick(dog.id)}>{dog.name}</span>
+    }
+    return null
+  })
+
   return (
     <div className="App">
       <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
+        <button id="good-dog-filter" onClick={handleUpdateGoodDogFilter}>Filter good dogs: {goodDogFilter ? "ON" : "OFF"}</button>
       </div>
-      <div id="dog-bar"></div>
+      <div id="dog-bar">
+        {dogBarItems}
+      </div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
+        <img src={dogClicked.image} alt={dogClicked.name} height="400px"/>
+        <h2>{dogClicked.name}</h2>
+        <button onClick={handleUpdateIsGood}>{dogClicked.isGoodDog ? "Bad Dog!" : "Good Dog!"}</button>
         <div id="dog-info"></div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
